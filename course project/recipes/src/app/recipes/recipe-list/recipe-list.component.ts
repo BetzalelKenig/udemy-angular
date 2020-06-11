93d67a6658d0e9edd15d4612a1eadf834786fb52
Betzalel Kenig <betzalelkenig@gmail.com>
2020-06-11 11:08:53 +0300

    end 53

--- course project/recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course project/recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is Just a test', 'https://149349705.v2.pressablecdn.com/wp-content/uploads/2015/02/theperfectloaf-top-3-leftover-sourdough-starter-recipes-2.jpg')
+  ];
 
   constructor() { }
 
