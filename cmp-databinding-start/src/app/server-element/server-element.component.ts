e4bf90fe65235b16819d17672c9651f74ef3a8c7
Betzalel Kenig <betzalelkenig@gmail.com>
2020-06-13 22:59:14 +0300

    end 83

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+  ElementRef,
+  ContentChild,
+  AfterViewInit
+ } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +17,31 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+OnInit,
+OnChanges,
+AfterViewInit {
   @Input( ) element: {type: string, name: string, content: string};
+  @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
+  constructor() {
+    console.log('constructor called!');
+  }
 
-  constructor() { }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called!');
+    console.log(this.header.nativeElement.textContent);
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!');
+    console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
 }
