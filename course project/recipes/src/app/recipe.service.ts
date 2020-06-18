934737ff5636b612bce66d88c25b198ce88bd4df
Betzalel Kenig <betzalelkenig@gmail.com>
2020-06-18 15:16:07 +0300

    finish 122

--- course project/recipes/src/app/recipe.service.ts
+++ course project/recipes/src/app/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipes/recipe.model';
+import { Ingredient } from './shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +8,14 @@ import { Recipe } from './recipes/recipe.model';
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is Just a test', 'https://www.wholesomeyum.com/wp-content/uploads/2017/03/wholesomeyum_low-carb-bread-recipe-almond-flour-bread-paleo-gluten-free.jpg'),
-    new Recipe('A Test Recipe 2', 'This is Just a test', 'https://www.wholesomeyum.com/wp-content/uploads/2017/03/wholesomeyum_low-carb-bread-recipe-almond-flour-bread-paleo-gluten-free.jpg'),
+    new Recipe('A Test Recipe', 'This is Just a test', 'https://www.wholesomeyum.com/wp-content/uploads/2017/03/wholesomeyum_low-carb-bread-recipe-almond-flour-bread-paleo-gluten-free.jpg', [
+      new Ingredient('Potato', 3),
+      new Ingredient('selery', 1)
+    ]),
+    new Recipe('A Test Recipe 2', 'This is Just a test', 'https://www.wholesomeyum.com/wp-content/uploads/2017/03/wholesomeyum_low-carb-bread-recipe-almond-flour-bread-paleo-gluten-free.jpg', [
+      new Ingredient('Coren', 2),
+      new Ingredient('Chiken', 10)
+    ]),
   ];
 
   getRecipes() {
